4510e8dc607defedec8128928b8253030f67e0b2
Cashku <cashkualb@gmail.com>
2024-08-06 20:49:11 +0200

    first commit

---
+++ src/component/firstComp.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const FirstComp = () => {
+  return <h2>First Component</h2>;
+};
---
+++ src/component/secondComp.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import img01 from "../component/crow-8838254_1280.jpg";
+const secondComp = () => {
+  return (
+    <div>
+      <img src={img01} alt="$" style={{ width: "100%", height: "400px" }} />
+    </div>
+  );
+};
+
+export default secondComp;
